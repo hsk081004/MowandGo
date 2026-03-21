dcf6ce2a9357a8e7ee8007f7787f9b2fcc9bf3ed
harsh kadikar <harshkadikar@gmail.com>
2026-03-21 13:38:43 +0530

    Final URL and fetch path fix

--- lawncare/urls.py
+++ lawncare/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from lawncare import views
 
 urlpatterns = [
+    path('admin/', admin.site.name_view),
     path('', views.home_view, name='home'),
     path('dashboard/', views.custom_dashboard, name='dashboard'),
-    path('dashboard/delete/<int:booking_id>/', views.delete_booking, name='delete_booking')
+    path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
 ]
