0dfc0d4a80b4d1440d24499601409c331df7f2ec
harsh kadikar <harshkadikar@gmail.com>
2026-03-21 12:40:35 +0530

    Switch to SSL for email

--- core/settings.py
+++ core/settings.py
@@ -135,9 +135,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # --- EMAIL SMTP CONFIGURATION ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465             # <-- Change to 465
+EMAIL_USE_SSL = True         # <-- Add this line (Set to True)
+EMAIL_USE_TLS = False        # <-- Change to False
 
-# This tells Django to securely grab your email and password from Render!
+# Keep these the same
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'harshkadikar@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
