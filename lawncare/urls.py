3ec5743884f8961f1312f956f63ce250b9fb35b2
harsh kadikar <harshkadikar@gmail.com>
2026-03-21 13:40:39 +0530

    Fix AdminSite attribute error and clean up URLs

--- lawncare/urls.py
+++ lawncare/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-from lawncare import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.name_view),
     path('', views.home_view, name='home'),
     path('dashboard/', views.custom_dashboard, name='dashboard'),
     path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
